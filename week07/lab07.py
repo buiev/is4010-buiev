a90e955fa305ecd3c83afb4e6cebc83bfd1031fa
Eric Bui <buiev@mail.uc.edu>
2026-03-01 18:32:45 -0500

    Lab07: add wrapper lab07.py and contact book implementation

---
+++ week07/lab07.py
@@ -0,0 +1,7 @@
+# wrapper for contact book functions
+from lab07_contact_book import save_contacts_to_json, load_contacts_from_json
+
+
+if __name__ == '__main__':
+    # simple smoke test
+    print("Contact book module loaded")
